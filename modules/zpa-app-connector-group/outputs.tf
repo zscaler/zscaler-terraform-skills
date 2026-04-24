0042d29dde88e931fb1d239b2877042e38aea48e
William Guilherme <wguilherme@securitygeek.io>
2026-04-24 15:08:42 -0700

    fix: Removed module directory

--- modules/zpa-app-connector-group/outputs.tf
+++
@@ -1,9 +0,0 @@
-output "id" {
-  description = "ID of the App Connector Group (used by zpa_server_group.app_connector_groups)."
-  value       = zpa_app_connector_group.this.id
-}
-
-output "name" {
-  description = "Name of the App Connector Group."
-  value       = zpa_app_connector_group.this.name
-}
--- modules/zpa-app-connector-group/versions.tf
+++
@@ -1,9 +0,0 @@
-terraform {
-  required_version = "~> 1.9"
-  required_providers {
-    zpa = {
-      source  = "zscaler/zpa"
-      version = "~> 4.0"
-    }
-  }
-}
